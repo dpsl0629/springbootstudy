7a45cc4b54198f971b86219833713a5eb971214c
dpsl0629 <dpsl0629@gmail.com>
2023-04-17 19:06:43 +0900

    Initial commit

---
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -0,0 +1,46 @@
+package org.zerock.board.service;
+
+import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Member;
+
+public interface BoardService {
+    Long register(BoardDTO dto);
+
+    void removeWithReplies(Long bno); // 삭제 기능
+
+    void modify(BoardDTO boardDTO);
+
+    default Board dtoToEntity(BoardDTO dto) {
+        Member member = Member.builder().email(dto.getWriterEmail()).build();
+
+        Board board = Board.builder()
+                .bno(dto.getBno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(member)
+                .build();
+        return board;
+    }
+
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO); // 목록 처리
+
+    BoardDTO get(Long bno);
+
+    default BoardDTO entityToDto(Board board, Member member, Long replyCount) {
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())
+                .build();
+
+        return boardDTO;
+    }
+}
